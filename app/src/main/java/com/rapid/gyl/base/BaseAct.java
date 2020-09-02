fe5ab751e81bd0bb3f59dd5dda60e5242dafb6f8
userGyl <user_gyl@163.com>
2020-09-02 11:11:18 +0800

    push acc test

--- app/src/main/java/com/rapid/gyl/base/BaseAct.java
+++ app/src/main/java/com/rapid/gyl/base/BaseAct.java
@@ -12,6 +12,6 @@ public class BaseAct extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //as
     }
 }
