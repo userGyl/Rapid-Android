1a6e27cd926954045d5b1047ca1c7656286cc6d2
userGyl <user_gyl@163.com>
2020-09-02 10:36:41 +0800

    push test

--- app/src/main/java/com/rapid/gyl/ui/MainActivity.java
+++ app/src/main/java/com/rapid/gyl/ui/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends BaseAct {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //PUSH TEST
     }
 }
